2d365a64cac521ec535b98e3c28cf33afbb0aec8
Matheus Oliveira <matheusvp2@ucl.br>
2023-05-28 19:07:13 -0300

    Adicionando mais campos de retorno

--- src/controller/alimentos.controller.js
+++ src/controller/alimentos.controller.js
@@ -2,9 +2,16 @@ import knex from "../database/index.js";
 
 export default class AlimentosController {
 
-    async get(req, res) {
+    async get (req, res) {
         const result = await knex.select("*").table("alimentos")
-        const response = result.map((item) => { return { id: item.id, nome: item.alimento } })
+        const response = result.map((item) => {
+            return {
+                id: item.id,
+                nome: item.alimento,
+                temperatura: item.temperatura,
+                tempo: item.tempo,
+            }
+        })
         return res.json({ data: response }).status(200);
     }
 
