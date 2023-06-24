b6cf536b52caf1b30d11fbcf1e008688e812aaa3
Matheus Oliveira <matheusvp2@ucl.br>
2023-06-23 23:02:15 -0300

    Novo Banco AWS

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,8 @@ const connection = {
     port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    ssl: { rejectUnauthorized: false }
 }
 
 const knex = knexInstance({
